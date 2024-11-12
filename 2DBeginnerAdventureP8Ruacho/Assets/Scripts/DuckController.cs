435a3526febebb3900e05bbcf0b2fb3365c81263
Ruaamanda <761383@student.dusd.net>
2024-11-12 15:30:01 -0800

    Sprite Animations and World Interactions
    On 2DBeginnerAdventureProject, I added sprite animations to the enemy, and Ducko (Player). Now, I'm on World Interactions and Ducko can now throw an object to eliminate threatful things.

--- 2DBeginnerAdventureP8Ruacho/Assets/Scripts/DuckController.cs
+++ 2DBeginnerAdventureP8Ruacho/Assets/Scripts/DuckController.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DuckController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+
+    public GameObject projectilePrefab;
+
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -18,12 +22,16 @@ public class DuckController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
 
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
 
     }
 
@@ -33,6 +41,17 @@ public class DuckController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             invincibleTimer -= Time.deltaTime;
@@ -40,6 +59,10 @@ public class DuckController : MonoBehaviour
             {
                 isInvincible = false;
             }
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
         }
 
     }
@@ -55,6 +78,8 @@ public class DuckController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
+
             if (isInvincible)
             {
                 return;
@@ -64,5 +89,14 @@ public class DuckController : MonoBehaviour
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
