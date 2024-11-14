868688d7d28953d534ace2de2148a57bf0e83d80
Ruaamanda <761383@student.dusd.net>
2024-11-14 15:26:13 -0800

    2DBeginnerAdventure
    I completed World Interactions, allowing Ducko to throw an object at the robots who cause damage. And, created a camera so that it follows wherever Ducko goes.

--- 2DBeginnerAdventureP8Ruacho/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Ruacho/Assets/Scripts/EnemyController.cs
@@ -11,6 +11,8 @@ public class EnemyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    bool broken = true;
+
     float timer;
     int direction = 1;
 
@@ -27,6 +29,11 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
         if (timer < 0)
         {
@@ -37,6 +44,10 @@ public class EnemyController : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if(!broken)
+        {
+            return;
+        }
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
@@ -61,5 +72,13 @@ public class EnemyController : MonoBehaviour
             player.ChangeHealth(-1);
         }
     }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
+    }
+
 }
   
